815a771bfc8dcf19ca24f9ee490f71d9d75f7295
diala2 <dialafayoumi@gmail.com>
2024-10-20 10:48:46 +0300

    Initial Commit

---
+++ Hydration/HydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationApp.swift
+//  Hydration
+//
+//  Created by Diala Abdulnasser Fayoumi on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HydrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
